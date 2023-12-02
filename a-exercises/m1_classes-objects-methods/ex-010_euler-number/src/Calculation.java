667937e0342c8424e8233dba2d6d978b8b363153
Anderson Sipriano <Andsipriano@gmail.com>
2023-12-01 21:17:20 -0300

    Ex10: Create a final static attribute

---
+++ a-exercises/m1_classes-objects-methods/ex-010_euler-number/src/Calculation.java
@@ -0,0 +1,5 @@
+public class Calculation {
+
+    //In this way, this variable will not change, and will be in every instance of calculation class
+    private static final double euler = Math.E;
+}
