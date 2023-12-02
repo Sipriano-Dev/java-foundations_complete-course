ae2de49ad6d7036b6e98a9e467a738598f00778b
Anderson Sipriano <Andsipriano@gmail.com>
2023-12-01 21:23:29 -0300

    Removing white spaces - strip()

--- people/src/LearnStrings.java
+++ people/src/LearnStrings.java
@@ -1,10 +1,36 @@
+import java.util.Arrays;
 
 public class LearnStrings {
     public static void main(String[] args) {
 
+        //Cutting whitespaces
+        String text = "   Anderson    ";
+        System.out.printf("'%s'", text.strip());
+        System.out.println();
+        System.out.printf("'%s'", text.stripLeading());
+        System.out.println();
+        System.out.printf("'%s'", text.stripTrailing());
 
-        String text = "Here is my awesome firf text";
-        System.out.println(text.replace('e', 'a'));
-        System.out.println(text.replace("firf", "nice"));
+        //Cutting whitespaces while indent the code, but ide already do it for us
+        String newText = """
+                <html>
+                    <div> Here is a div space </div>
+                </html>
+                """;
+
+        System.out.println();
+        System.out.println("--------------");
+
+        System.out.println(newText.stripIndent());
+
+        //Show what the method returns
+        System.out.printf("'%s'", stripText(text));
+
+
+    }
+
+    //Creating our own strip method
+    public static String stripText(String text) {
+        return text.replace(" ", "");
     }
 }
