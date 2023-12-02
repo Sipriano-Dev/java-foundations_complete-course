b87509e72dd65778c49f6580e656b1eb341ac7bc
Anderson Sipriano <Andsipriano@gmail.com>
2023-12-01 21:13:45 -0300

    Ex7: Create a Car class and instantiate a print with toString

---
+++ a-exercises/m1_classes-objects-methods/ex-007_garage/src/Main.java
@@ -0,0 +1,9 @@
+import model.Car;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car("Civic", "Black");
+        System.out.println(car);
+    }
+}
---
+++ a-exercises/m1_classes-objects-methods/ex-007_garage/src/model/Car.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Car {
+
+    private String model;
+    private String color;
+
+    public Car(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void moving() {
+        System.out.println("Car is moving");
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
