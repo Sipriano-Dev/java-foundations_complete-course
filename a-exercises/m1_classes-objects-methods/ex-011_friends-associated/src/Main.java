90d455ad97ceedbec31a6e0311900e2978f19476
Anderson Sipriano <Andsipriano@gmail.com>
2023-12-01 21:18:28 -0300

    Ex11: Create a static array to share data between instances

---
+++ a-exercises/m1_classes-objects-methods/ex-011_friends-associated/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        People f1 = new People();
+        People f2 = new People();
+        People f3 = new People();
+
+        System.out.println(f1.getFriends()[0]);
+        System.out.println(f1.getFriends()[1]);
+        System.out.println(f1.getFriends()[2]);
+
+        System.out.println(f2.getFriends()[0]);
+        System.out.println(f2.getFriends()[1]);
+        System.out.println(f2.getFriends()[2]);
+
+        System.out.println(f3.getFriends()[0]);
+        System.out.println(f3.getFriends()[1]);
+        System.out.println(f3.getFriends()[2]);
+
+    }
+}
---
+++ a-exercises/m1_classes-objects-methods/ex-011_friends-associated/src/People.java
@@ -0,0 +1,11 @@
+public class People {
+    private static final String[] friends = {"Mike", "John", "Jake"};
+    private String other;
+
+    public People() {
+    }
+
+    public String[] getFriends() {
+       return People.friends;
+    }
+}
