9d129c634941acb701fa769e318a66b20f3a27e6
Anderson Sipriano <Andsipriano@gmail.com>
2023-12-01 21:28:11 -0300

    Determining the length of a String

--- people/src/LearnStrings.java
+++ people/src/LearnStrings.java
@@ -3,30 +3,22 @@ import java.util.Arrays;
 public class LearnStrings {
     public static void main(String[] args) {
 
-        String myText = "This is my text 1";
-        String myText2 = "This is my text 2";
-//        System.out.println(myText + " " + myText2);
+//        String myText = "For score and seven years ago";
+//        System.out.println(myText.length());
+//        char[] charArray = myText.toCharArray();
+//        int index = 30;
+//        if (index < myText.length()) {
+//            System.out.println(charArray[index]);
 
-//        System.out.println(myText.concat(myText2));
-//        System.out.println("My String".concat("other literal"));
+        String text = "For";
+        String text2 = "score";
 
-        String finalString = new StringBuilder(myText.length() + 1 + myText2.length())
-                .append(myText)
-                .append(" ")
-                .append(myText2)
-                .toString();
-        System.out.println(finalString);
+        StringBuilder builder = new StringBuilder(text.length() + text2.length())
+                .append(text)
+                .append(text2);
+        System.out.println(builder.length());
 
-        String otherFinalString = new StringBuffer()
-                .append(myText)
-                .append(" ")
-                .append(myText2)
-                .toString();
-        System.out.println(finalString);
 
-        System.out.format("%s %s\n", myText, myText2);
-        String finalSring2 = String.format("%s %s\n", myText, myText2);
-        System.out.println(finalSring2);
 
     }
 
