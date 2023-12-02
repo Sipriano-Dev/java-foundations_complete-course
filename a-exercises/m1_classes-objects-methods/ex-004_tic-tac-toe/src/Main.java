f23c67d4205173ef5e72a271bb7b5874b6204413
Anderson Sipriano <Andsipriano@gmail.com>
2023-12-01 21:10:09 -0300

    Ex4: Create a tic tac toe array

---
+++ a-exercises/m1_classes-objects-methods/ex-004_tic-tac-toe/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String[][] ticTac = new String[][]{{"O", "X", "X"}, {"X", "O", "O"}, {"X", "O", "O"}};
+        System.out.println(ticTac[2][2]);
+    }
+}
