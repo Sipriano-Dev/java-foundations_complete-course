bd95c5b6615d3e5ffb50fed71f37d46a9489baac
Anderson Sipriano <Andsipriano@gmail.com>
2023-12-01 21:06:07 -0300

    Ex2: Create an array of the week and use it

---
+++ a-exercises/m1_classes-objects-methods/ex-002_week-array/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] week = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        System.out.println(week.length);
+        System.out.println(week[3]);
+    }
+}
