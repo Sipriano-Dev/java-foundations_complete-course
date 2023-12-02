479277fd7f6221cba20c25698e62435f6bc1c29e
Anderson Sipriano <Andsipriano@gmail.com>
2023-12-01 21:11:40 -0300

    Ex5: Create a method to receive an array without instantiate

---
+++ a-exercises/m1_classes-objects-methods/ex-005_vect-in-method/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        vect("Gabrielle", "Anderson");
+
+    }
+
+    public static void vect(String... v) {
+        System.out.println(v[0]);
+    }
+
+}
