a8a4abaee7dba4dae02eaccb47334d99b409033f
Anderson Sipriano <Andsipriano@gmail.com>
2023-12-01 21:33:39 -0300

    Beginning & Ending of Strings - startWith() & endsWith()

--- people/src/LearnStrings.java
+++ people/src/LearnStrings.java
@@ -3,27 +3,15 @@ import java.util.Arrays;
 public class LearnStrings {
     public static void main(String[] args) {
 
-        //LEARN HOW TO SPLIT STRINGS
+        //LEARN startWith() AND endsWith() METHODS
 
-        String text = """
-                Sipriano,Anderson,1/1/88,89 abc st.,apple,ca
-                Ferreira,Gabriell,2/3/95,90 cba st.,orange,sp
-                Lourenço,Felipe,3/3/05,91 abc st.,pineapple,sc
-                """;
-        //Create a string of array that will receive anoter array that the split metho will return
-        //based in a regex
-        String[] people = text.split("\n");
+        String text = "   file001.txt";
 
-        //Tell how many people in the array
-        System.out.println(people.length);
+        //Return a boolean if ends with the given text
+        System.out.println(text.endsWith("txt"));
 
-        //Call the first info people in the array
-        System.out.println(people[0]);
-
-        String[] person1 = people[2].split(",");
-
-        //Printing address
-        System.out.println(person1[3]);
+        //Return a boolean if start with the given text
+        System.out.println(text.strip().startsWith("file"));
     }
 
 
