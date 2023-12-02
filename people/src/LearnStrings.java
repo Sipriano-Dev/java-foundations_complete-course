230dd0b54cba6f0c686f52f5649c6b19a5e33f70
Anderson Sipriano <Andsipriano@gmail.com>
2023-12-01 21:25:57 -0300

    Determining if text is contained in a String - contains()

--- people/src/LearnStrings.java
+++ people/src/LearnStrings.java
@@ -3,19 +3,10 @@ import java.util.Arrays;
 public class LearnStrings {
     public static void main(String[] args) {
 
-        String firstWord = "Apple";
-        String secondWord = "apple";
+        String myText = "Four score and seven years ago";
+        System.out.println(myText.contains("seven"));
 
-        System.out.println(firstWord.compareTo(secondWord));
-        System.out.println(firstWord.compareToIgnoreCase(secondWord));
 
-        System.out.println(compareToIgnoreCase(firstWord, secondWord));
-
-    }
-
-    //My own compareToIgnoreCase method heehe
-    public static int compareToIgnoreCase(String word1, String word2) {
-        return word1.toUpperCase().compareTo(word2.toUpperCase());
     }
 
 }
