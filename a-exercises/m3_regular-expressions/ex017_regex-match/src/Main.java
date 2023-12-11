e10607e4ae712cf9f81425f8feade3fe9bd39d50
Anderson Sipriano <Andsipriano@gmail.com>
2023-12-11 00:08:35 -0300

    Ex17.1: Regex match stark

--- a-exercises/m3_regular-expressions/ex017_regex-match/src/Main.java
+++ a-exercises/m3_regular-expressions/ex017_regex-match/src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // 1. Write a regex that would match the following words: Dark, bark, Lark
+        //1. Write a regex that would match the following words: Dark, bark, Lark
         System.out.println("Lark".matches("\\wark"));
         // More specific
         System.out.println("Lark".matches("[LDb]ark"));
         // General, but not number
         System.out.println("Lark".matches("[a-zA-Z]ark"));
+
+        //1. For extra challenge, could you additionally make it match: stark
+        System.out.println("Stark".matches("\\w\\wark"));
+        System.out.println("Stark".matches("[a-zA-Z]{2}ark"));
+
     }
 }
