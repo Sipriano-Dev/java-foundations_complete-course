5bac13a467ac9cf847f01d22dee35c3a6db14616
Anderson Sipriano <Andsipriano@gmail.com>
2023-12-22 12:55:58 -0300

    Numeric Data Types

---
+++ people/src/NumberStuff.java
@@ -0,0 +1,12 @@
+public class NumberStuff {
+
+    public static void main(String[] args) {
+        byte myBite = 13; //Address is like @2ab4664f2
+        boolean myFlag = true; //The smallest number to store It takes only 1 bit, 1 or 0
+        short myShort = 45;// -32,768 to 32,767, 2 bytes = 16 bits - Signed
+        char myChar = '강'; //gang
+        char myA = 'a'; //65, char is Unsigned
+
+        System.out.println(myBite);
+    }
+}
