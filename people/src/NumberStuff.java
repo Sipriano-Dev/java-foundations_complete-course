7a81a187b928582296512b9d5f46d78c7e77c967
Anderson Sipriano <Andsipriano@gmail.com>
2023-12-22 14:45:28 -0300

    Floating Point Numbers

--- people/src/NumberStuff.java
+++ people/src/NumberStuff.java
@@ -1,12 +1,18 @@
 public class NumberStuff {
 
     public static void main(String[] args) {
-        byte myBite = 13; //Address is like @2ab4664f2
-        boolean myFlag = true; //The smallest number to store It takes only 1 bit, 1 or 0
-        short myShort = 45;// -32,768 to 32,767, 2 bytes = 16 bits - Signed
-        char myChar = '강'; //gang
-        char myA = 'a'; //65, char is Unsigned
+        int myInt = 43; // Range -2,147,483,648 to 2,147,483,647 (2 bilions) 4 bytes = 32 bits
+        long myLont = 3566; //Range -9,223,372,036,854,775,808 to 9,223.372,036,854,775,807 (8 Byte = 64 bits)
+        double myDouble = 3.141592; // The IDE take double as default
+        float myFloat = 3.141592f; // Because of this, you put an f to indicate it is a float, not double
 
-        System.out.println(myBite);
+        double num1 = 2.15;
+        double num2 = 1.10;
+        System.out.println(num1 - num2); //1.0499999999999998, it is supposed to print a 1.05
+
+        float num3 = 2.15f;
+        float num4 = 1.10f;
+        System.out.println(num3 - num4); //1.0500001, it always be aproximated result,
+
     }
 }
