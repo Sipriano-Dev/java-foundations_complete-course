5d15524d6a3fb2ee7de65f51ec9ec12e774093b0
Anderson Sipriano <Andsipriano@gmail.com>
2024-01-09 06:08:06 -0300

    Standard Math Functions

--- people/src/MathStuff.java
+++ people/src/MathStuff.java
@@ -1,70 +1,17 @@
 public class MathStuff {
 
     public static void main(String[] args) {
-        System.out.println(3 + 5);
-        System.out.println(3 - 5);
-        System.out.println(3 * 5);
-        System.out.println(3 / 5);
-        //Precedence order here
-        // 1. * /
-        // 2. + -
+        System.out.println(Math.abs(-5)); //Returns the absolute number
+        System.out.println(Math.ceil(5.1)); //It will round up to 6
+        System.out.println(Math.floor(5.9)); //It will round down to 5
+        System.out.println(Math.min(3, 6)); //It will return the smallest number
+        System.out.println(Math.max(3, 6)); //It will return the biggest number
+        System.out.println(Math.pow(2, 3)); //To power a number
 
-        System.out.println(7 / 4); //will be 1, because it is two integer, so the result will be integer
-        // to solve this, you have to put a double or float in one of two number or both 7.0 / 4 = 1.75
-        // of an f 7f / 4 = 1.75
-        // or cast (double)7f / 4 = 1.75
+        System.out.println(Math.random()); //To Random number, but for simple things, not trust for important things
+        System.out.println((int)Math.round((Math.random() * 25))); //Improve to get from 1 to 25 with int result
+        // Note that is surround by (), because the result * 26 will be casting
+        // Math.round() round a number if is 1.5 or more will be rounded to 2, if it is 1.49 or less will be 1
 
-        System.out.println((double)(7 / 4)); // In this case the result will be 1.0 because first will be a calculation
-        // and the result will be casting
-
-        System.out.println(5 * ((3d + 2d) / 10)); // You can put a d to say it is a double number
-        //Precedence order here
-        // 1. (){}
-        // 2. * /
-        // 3. + -
-
-        System.out.println(4 % 2); //This is a module operation, will return the rest of the division, 0 in this case
-
-        // You can use a module operation to make a pattern saying that the sequence wil be 1 and 0 1 and 0
-        System.out.println(1 % 2);
-        System.out.println(2 % 2);
-        System.out.println(3 % 2);
-        System.out.println(4 % 2);
-        System.out.println(5 % 2);
-        System.out.println(6 % 2);
-        System.out.println(7 % 2);
-        System.out.println(8 % 2);
-        System.out.println(9 % 2);
-        System.out.println(10 % 2);
-
-        // Or in each 3 numbers will be a pattern of 0, 1, 2
-        System.out.println(1 % 3);
-        System.out.println(2 % 3);
-        System.out.println(3 % 3);
-        System.out.println(4 % 3);
-        System.out.println(5 % 3);
-        System.out.println(6 % 3);
-        System.out.println(7 % 3);
-        System.out.println(8 % 3);
-        System.out.println(9 % 3);
-        System.out.println(10 % 3);
-
-        System.out.println("-----------------------");
-        int x = 1;
-        System.out.println(x);
-        //x = x + 1;
-        //x+=1;
-        x++; //Just for one you can use this, for others, use this x+=3
-        System.out.println(x);
-
-        System.out.println("----------------------------");
-        System.out.println(x++); //Will print and after that will add a number 1
-        System.out.println(++x); //Will add a number 1 and after that will print
-
-        System.out.println("--------------------------------");
-        boolean myFlag = true;
-        System.out.println(myFlag);
-        myFlag = !myFlag;
-        System.out.println(myFlag); //! means not, reverse the result, flipping it
     }
 }
