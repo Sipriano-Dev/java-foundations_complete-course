6d155580a927d0eb0573e55854125f028d3bff1a
Anderson Sipriano <Andsipriano@gmail.com>
2024-01-09 12:45:57 -0300

    Introducing BigDecimal

--- people/src/NumberStuff.java
+++ people/src/NumberStuff.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.security.SecureRandom;
 import java.util.Random;
 
@@ -5,12 +6,18 @@ public class NumberStuff {
 
     public static void main(String[] args) {
 
-        Random random = new Random(3436346); //seed if you want
-        System.out.println(random.nextInt(25)); //It better than Math.random()
 
-        SecureRandom secureRandom = new SecureRandom();
-        System.out.println(secureRandom.nextInt(25));
-        //Can put a seed too, it's like random but better in security
+        float num1 = 2.15f;
+        float num2 = 1.10f;
+        System.out.println(num1 - num2);
+
+        //Best way to instantiate a bigdecimal is with string, to avoid the unprecise round
+        BigDecimal num3 = new BigDecimal("2.15");
+        BigDecimal num4 = new BigDecimal("1.10");
+        //System.out.println(num3 - num4); //You can't do this because it will work only with primitive
+        System.out.println(num3.subtract(num4));
+        //When you want precision, to money for example, uses big decimal
+        //The use of the float and double, is because it is faster
 
     }
 }
