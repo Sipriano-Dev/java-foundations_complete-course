8f53829e00ce2a7c3dec672b6ca171f8257030c3
Anderson Sipriano <Andsipriano@gmail.com>
2024-01-09 03:12:21 -0300

    Beyond Decimal

--- people/src/NumberStuff.java
+++ people/src/NumberStuff.java
@@ -1,18 +1,14 @@
 public class NumberStuff {
 
     public static void main(String[] args) {
-        int myInt = 43; // Range -2,147,483,648 to 2,147,483,647 (2 bilions) 4 bytes = 32 bits
-        long myLont = 3566; //Range -9,223,372,036,854,775,808 to 9,223.372,036,854,775,807 (8 Byte = 64 bits)
-        double myDouble = 3.141592; // The IDE take double as default
-        float myFloat = 3.141592f; // Because of this, you put an f to indicate it is a float, not double
 
-        double num1 = 2.15;
-        double num2 = 1.10;
-        System.out.println(num1 - num2); //1.0499999999999998, it is supposed to print a 1.05
+        byte anotherByte = 0x1f; //0x says that it will be an hexadecimal number, in this case will be 31 because
+        //when is before another number represents 16 + f(15)
+        int anotherInt = 0b01 | 0b10 | 0b100; //0b says that it'll be a binary number, start from right to left
+        //The or | says if it has a 1 than the final result is gonna be 1
 
-        float num3 = 2.15f;
-        float num4 = 1.10f;
-        System.out.println(num3 - num4); //1.0500001, it always be aproximated result,
+        System.out.println(anotherInt);
+        System.out.println(anotherByte);
 
     }
 }
