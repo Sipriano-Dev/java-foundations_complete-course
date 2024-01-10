c73437f0240b85819cbb25aead988a683ca7bc46
Anderson Sipriano <Andsipriano@gmail.com>
2024-01-10 11:49:08 -0300

    Using BigDecimal

--- people/src/NumberStuff.java
+++ people/src/NumberStuff.java
@@ -1,4 +1,6 @@
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.security.SecureRandom;
 import java.util.Random;
 
@@ -6,18 +8,31 @@ public class NumberStuff {
 
     public static void main(String[] args) {
 
+        //You need this, because maybe this operation will return a long decimal number, so you have to specify a cut off
+        //You use this context to solve the error down below
+        //You can put that inline too, without instantiate
+        MathContext mc = new MathContext(5, RoundingMode.HALF_UP);
 
-        float num1 = 2.15f;
-        float num2 = 1.10f;
-        System.out.println(num1 - num2);
+        //Will be an error, you need to specify a
+        //quantity of decimals
+        System.out.println(new BigDecimal("35").divide(new BigDecimal(".46"), mc));
 
-        //Best way to instantiate a bigdecimal is with string, to avoid the unprecise round
-        BigDecimal num3 = new BigDecimal("2.15");
-        BigDecimal num4 = new BigDecimal("1.10");
-        //System.out.println(num3 - num4); //You can't do this because it will work only with primitive
-        System.out.println(num3.subtract(num4));
-        //When you want precision, to money for example, uses big decimal
-        //The use of the float and double, is because it is faster
+        //You have to say the math context to get sqrt
+        System.out.println(new BigDecimal("7").sqrt(mc));
+
+        System.out.println(new BigDecimal("7").max(new BigDecimal("9")));
+
+        //This is equal to % module
+        System.out.println(new BigDecimal("7").remainder(new BigDecimal("5")));
+
+        //And you can use as a pattern
+        System.out.println("-------------------------------");
+        System.out.println(new BigDecimal("1").remainder(new BigDecimal("2")));
+        System.out.println(new BigDecimal("2").remainder(new BigDecimal("2")));
+        System.out.println(new BigDecimal("3").remainder(new BigDecimal("2")));
+        System.out.println(new BigDecimal("4").remainder(new BigDecimal("2")));
+
+
 
     }
 }
