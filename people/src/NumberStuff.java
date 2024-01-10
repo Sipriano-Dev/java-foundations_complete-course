8b15d56c75d6fbc4ec8d0a9941f75cea239527de
Anderson Sipriano <Andsipriano@gmail.com>
2024-01-10 19:09:37 -0300

    Numeric Wrapper Classes

--- people/src/NumberStuff.java
+++ people/src/NumberStuff.java
@@ -1,35 +1,32 @@
-import java.math.BigDecimal;
-import java.math.MathContext;
-import java.math.RoundingMode;
-import java.security.SecureRandom;
-import java.util.Random;
 
 public class NumberStuff {
 
     public static void main(String[] args) {
 
-        //Always use this, with string if you want precision
-        BigDecimal num = new BigDecimal("127");
+        int myInt = 7;
+        //Auto-boxing, but you don't need, you can do it direct
+        //Integer myInteger = num1; java do auto-boxing automatically
+        //You can use Wrapper classes with double, float an son on
+        Integer myInteger = Integer.valueOf(myInt);
+        //In this case, you need the valueOf, because you are passing a string
+        //With byte, is necessary using an string
+        Double myDouble = Double.valueOf("2.0");
+        storeData(myInt);
 
-        //This way java will use primitive in memory, losing some precision in decimals, and will be slower than
-        //Primitive, since you will instantiate a big decimal
-        BigDecimal num2 = new BigDecimal(333);
+        //This is how you can parse a String to a int, or a double or a boolena, etc
+        //Internally, Integer is using valueOf to parse Integer to int
+        int age = Integer.parseInt(args[0]);
 
-        //Some more ways to instantiate a Big decimal
-        BigDecimal num3 = new BigDecimal(333f);
-        int num4 = 4;
-        BigDecimal num5 = new BigDecimal(num4);
-
-        //You can get the big decimal to other type like int, double, etc
-        byte myBite = num.byteValue();
-        System.out.println(myBite);
-
-        //Bring the big decimal to string, even if it was instantiated in a String way, it is a big decimal
-        String myString = num.toString();
-
-
-
+        System.out.println(age + 15);
 
 
     }
+
+    /**
+     * Takes in any type of data and stores in somewhere generically
+     */
+    public static void storeData(Object obj) {
+        System.out.println("ola");
+    }
+
 }
