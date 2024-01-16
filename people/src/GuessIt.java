de709f1e12c4e6656e0276ba19b82a65d18d9bdb
Anderson Sipriano <Andsipriano@gmail.com>
2024-01-16 18:28:25 -0300

    The Switch Statement

--- people/src/GuessIt.java
+++ people/src/GuessIt.java
@@ -6,12 +6,45 @@ public class GuessIt {
         int randomNum = new Random().nextInt(1, 4);
         System.out.printf("The random number is: %d%n", randomNum);
 
-        if (randomNum < 2) {
-            System.out.println("The color is RED");
-        } else if (randomNum > 4 && (randomNum % 2 == 0)) {
-            System.out.println("The color is BLUE");
-        } else {
-            System.out.println("The color is GREEN");
+//        if (randomNum == 1) {
+//            System.out.println("The color is RED");
+//        } else if (randomNum == 2) {
+//            System.out.println("The color is BLUE");
+//        } else if (randomNum == 3) {
+//            System.out.println("The color is GREEN");
+//        } else {
+//            System.out.println("The color is Black");
+//        }
+
+        //It works with String and other types too
+        switch (randomNum) {
+            case 1:
+                System.out.println("The color is RED");
+                break;
+            case 2:
+                System.out.println("The color is BLUE");
+                break;
+            case 3:
+                System.out.println("The color is GREEN");
+                break;
+            case 4:
+                System.out.println("The color is BLACK");
+                break;
+            default:
+                System.out.println("The color is PURPLE");
+                break;
         }
+
+        String command = null;
+        //Don't use this
+//        if (command.equals("Stop")) {
+//            System.out.println("will be an exception");
+//        }
+
+        //Use like this
+        if ("Stop".equals(command)) {
+            System.out.println("This way will not be an error");
+        }
+
     }
 }
