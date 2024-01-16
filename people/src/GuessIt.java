c7d3a2bd78b5e7beadd75904269f7763c9ed8fd6
Anderson Sipriano <Andsipriano@gmail.com>
2024-01-16 20:06:22 -0300

    Switch Statement: New Features

--- people/src/GuessIt.java
+++ people/src/GuessIt.java
@@ -3,48 +3,24 @@ import java.util.Random;
 public class GuessIt {
 
     public static void main(String[] args) {
-        int randomNum = new Random().nextInt(1, 4);
-        System.out.printf("The random number is: %d%n", randomNum);
 
-//        if (randomNum == 1) {
-//            System.out.println("The color is RED");
-//        } else if (randomNum == 2) {
-//            System.out.println("The color is BLUE");
-//        } else if (randomNum == 3) {
-//            System.out.println("The color is GREEN");
-//        } else {
-//            System.out.println("The color is Black");
-//        }
+        String card = "ace";
+        int currentTotalValue = 15;
 
-        //It works with String and other types too
-        switch (randomNum) {
-            case 1:
-                System.out.println("The color is RED");
-                break;
-            case 2:
-                System.out.println("The color is BLUE");
-                break;
-            case 3:
-                System.out.println("The color is GREEN");
-                break;
-            case 4:
-                System.out.println("The color is BLACK");
-                break;
-            default:
-                System.out.println("The color is PURPLE");
-                break;
-        }
+        int currentValue = switch (card) {
+            case "king", "queen", "jack" -> 10;
+            case "ace" -> {
+                if (currentTotalValue < 11) {
+                    yield 11;
+                } else {
+                    yield 1;
+                }
+            }
+            default -> Integer.parseInt(card);
+        };
 
-        String command = null;
-        //Don't use this
-//        if (command.equals("Stop")) {
-//            System.out.println("will be an exception");
-//        }
-
-        //Use like this
-        if ("Stop".equals(command)) {
-            System.out.println("This way will not be an error");
-        }
+        System.out.printf("Current card value: %d%n", currentValue);
+        System.out.printf("Total card value: %d%n", currentTotalValue + currentValue);
 
     }
 }
