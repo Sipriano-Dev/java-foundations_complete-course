cfbaa9d7e8af2dd70b3b08a5fc4f893aa30f8923
Anderson Sipriano <andsipriano@gmail.com>
2024-02-16 18:27:13 -0300

    While Loops: Part One

--- people/src/GuessIt.java
+++ people/src/GuessIt.java
@@ -1,26 +1,23 @@
+import java.math.BigDecimal;
 import java.util.Random;
 
 public class GuessIt {
 
     public static void main(String[] args) {
 
-        String card = "ace";
-        int currentTotalValue = 15;
+        int randomNum = new Random().nextInt(1, 11);
 
-        int currentValue = switch (card) {
-            case "king", "queen", "jack" -> 10;
-            case "ace" -> {
-                if (currentTotalValue < 11) {
-                    yield 11;
-                } else {
-                    yield 1;
-                }
+        while (true) {
+            String guessedNumberString = System.console().readLine("Please enter with a number between 1 and 10: ");
+            int guessedNumber = Integer.parseInt(guessedNumberString);
+
+            if (guessedNumber == randomNum) {
+                System.out.printf("The random number is %d, you win%n", randomNum);
+                return;
+            } else {
+                System.out.printf("you are wrong, try again%n", randomNum);
             }
-            default -> Integer.parseInt(card);
-        };
-
-        System.out.printf("Current card value: %d%n", currentValue);
-        System.out.printf("Total card value: %d%n", currentTotalValue + currentValue);
+        }
 
     }
 }
