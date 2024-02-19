c059aafdb910f442a8e2826dde45b319278b9f0a
Anderson Sipriano <andsipriano@gmail.com>
2024-02-19 17:07:10 -0300

    While Loops: Part Two

--- people/src/GuessIt.java
+++ people/src/GuessIt.java
@@ -6,16 +6,22 @@ public class GuessIt {
     public static void main(String[] args) {
 
         int randomNum = new Random().nextInt(1, 11);
+        String guessedNumberString = null;
+        //This way there's gonna be an exception, because guessedNumberString is null, and cannot call a method equals
+        //The solution is to invert and call equal from "q"
+//        while (!guessedNumberString.equals("q")) {
+        while (!"q".equals(guessedNumberString)) {
+            guessedNumberString = System.console().readLine("Please enter with a number between 1 and 10: ");
 
-        while (true) {
-            String guessedNumberString = System.console().readLine("Please enter with a number between 1 and 10: ");
-            int guessedNumber = Integer.parseInt(guessedNumberString);
 
-            if (guessedNumber == randomNum) {
-                System.out.printf("The random number is %d, you win%n", randomNum);
-                return;
-            } else {
-                System.out.printf("you are wrong, try again%n", randomNum);
+            if (guessedNumberString.matches("-?\\d+")) {
+                int guessedNumber = Integer.parseInt(guessedNumberString);
+                if (guessedNumber == randomNum) {
+                    System.out.printf("The random number is %d, you win%n", randomNum);
+                    return;
+                } else {
+                    System.out.printf("you are wrong, try again%n", randomNum);
+                }
             }
         }
 
