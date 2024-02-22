d9115657b4d1d760f07355a2deebe0482a16a8e2
Anderson Sipriano <andsipriano@gmail.com>
2024-02-22 18:29:56 -0300

    Do/While Loops: Part Two

--- people/src/GuessIt.java
+++ people/src/GuessIt.java
@@ -1,17 +1,18 @@
-import java.math.BigDecimal;
 import java.util.Random;
 
 public class GuessIt {
 
+    private static final int LIMIT_ALLOWED_TRIED = 5; //Avoid magic numbers
+
     public static void main(String[] args) {
 
         int randomNum = new Random().nextInt(1, 11);
-        System.out.printf("The random number is %d.%n", randomNum);
         String guessedNumberString = null;
         //This way there's gonna be an exception, because guessedNumberString is null, and cannot call a method equals
         //The solution is to invert and call equal from "q"
 //        while (!guessedNumberString.equals("q")) {
         int wrongGuesCount = 1;
+        System.out.printf("The random number is %d.%n", randomNum);
         do {
             //int wrongGuesCount = 1;//This will not count, because every time will set to 1 again
             //and it will exists only inside of the block
@@ -26,7 +27,14 @@ public class GuessIt {
                     return; //That finish the do/while code
                 } else {
                     wrongGuesCount++;
-                    System.out.printf("You didn't get it, try again%n");
+                    if (wrongGuesCount <= LIMIT_ALLOWED_TRIED) {
+                        //It is good to use var instead of magic number
+                        System.out.printf("You didn't get it, try again, tries: %d of 5 %n", wrongGuesCount-1);
+                    } else {
+                        System.out.println("5 of 5 tries, you loose");
+                        System.out.printf("The random number is %d.%n", randomNum);
+                        return;
+                    }
 
                 }
             }
