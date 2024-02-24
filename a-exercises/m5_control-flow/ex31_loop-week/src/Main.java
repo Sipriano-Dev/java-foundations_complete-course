2c14247efd9bbc8a1c061142c6c9c0adf9412cfb
Anderson Sipriano <Andsipriano@gmail.com>
2024-02-24 16:33:33 -0300

    Ex31: Loop Days of The Week

---
+++ a-exercises/m5_control-flow/ex31_loop-week/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        for (int i = 0; i <  week.length; i++) {
+            System.out.println(week[i]);
+        }
+
+    }
+}
