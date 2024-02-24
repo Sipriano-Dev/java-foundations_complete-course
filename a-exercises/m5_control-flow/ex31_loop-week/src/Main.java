92ff272834396497cc63d45378ce1269d8211a92
Anderson Sipriano <Andsipriano@gmail.com>
2024-02-24 17:22:40 -0300

    Ex31.3: Do it With Ternary If

--- a-exercises/m5_control-flow/ex31_loop-week/src/Main.java
+++ a-exercises/m5_control-flow/ex31_loop-week/src/Main.java
@@ -6,12 +6,9 @@ public class Main {
         String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
         for (int i = 0; i < week.length; i++) {
-            if (i % 2 != 0) {
-                System.out.println(week[i].toUpperCase());
-            } else {
-                System.out.println(week[i]);
-            }
 
+            String output = i % 2 != 0 ? week[i].toUpperCase() : week[i];
+            System.out.println(output);
         }
 
     }
