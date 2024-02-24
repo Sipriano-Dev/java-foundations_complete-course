f8cc7cfc64efa97e2aa01c8fc7c334a14d2f19c1
Anderson Sipriano <Andsipriano@gmail.com>
2024-02-24 15:37:30 -0300

    A Simpler For Loop

---
+++ people/src/ForLoopExample.java
@@ -0,0 +1,7 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        for (int i=0; i<=10; i++) {
+            System.out.printf("The counter is %d%n", i);
+        }
+    }
+}
