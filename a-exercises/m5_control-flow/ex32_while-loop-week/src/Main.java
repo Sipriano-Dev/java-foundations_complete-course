e2d0444a5d870831a9b70f7ce359fbda11df34ff
Anderson Sipriano <Andsipriano@gmail.com>
2024-02-26 15:37:30 -0300

    Ex32: Loops Days of the Week with while

---
+++ a-exercises/m5_control-flow/ex32_while-loop-week/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        int position = 0;
+        while (position < 7) {
+            System.out.println(week[position]);
+            position++;
+        }
+
+    }
+}
